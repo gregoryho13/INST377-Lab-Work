148a67c64c831d4111b45b5c19d3aaa202e9de0e
gregoryho13 <98433943+gregoryho13@users.noreply.github.com>
2023-02-19 21:43:48 -0500

    lab_3

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -7,7 +7,7 @@
 */
 /* eslint-enable max-len */
 // set our first slide's position to "0", the opening position in an array
-const slidePosition = 0;
+let slidePosition = 0;
 
 // gather a reference to every slide we're using via the class name and querySelectorAll
 const slides = document.querySelectorAll('.carousel_item');
@@ -23,9 +23,15 @@ function updateSlidePosition() {
   // loop through all the slides in your slideArray
   // and remove the 'visible' class from each classList
   // then add a class 'hidden' to all of them
+  slidesArray.forEach((slide) => {
+    slide.classList.remove('visible');
+    slide.classList.add('hidden');
+  })
 
   // outside your .forEach,
   // add a 'visible' class to the slide at the current slidePosition in slides
+  console.log(slidePosition)
+  slides[slidePosition].classList.add('visible')
 }
 
 function moveToNextSlide() {
@@ -35,6 +41,11 @@ function moveToNextSlide() {
     and if so, sets your slidePosition to the first index of an array
     if not, set the slidePosition to the current position plus one
   */
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0; 
+  } else {
+    slidePosition += 1;
+  }
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
@@ -45,6 +56,11 @@ function moveToPrevSlide() {
     and if so, sets your slidePosition to the last slide position in totalSlides
     if not, set the slidePosition to the current position minus one
   */
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition -= 1;
+  }
   updateSlidePosition();
 }
 
@@ -60,3 +76,8 @@ document.querySelector('.next') // Get the appropriate element (<button class="n
 
 // Paying close attention to the above queryselector, write one that fires
 // when you want a "prev" slide
+document.querySelector('.prev')
+  .addEventListener('click', () => {
+    console.log('clicked prev');
+    moveToPrevSlide();
+  })
