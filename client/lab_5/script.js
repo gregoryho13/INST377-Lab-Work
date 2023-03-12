151659cc6e2db2f4fbedb758cd67bd6c3ac5b40b
gregoryho13 <98433943+gregoryho13@users.noreply.github.com>
2023-03-12 16:36:17 -0400

    lab5 api script

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -12,10 +12,16 @@ function filterList(list, query) {
 
     Ask the TAs if you need help with this
   */
+  return list.filter((item) => {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = query.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+  });
 }
 
 async function mainEvent() { // the async keyword means we can make API requests
   const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
+  const filterButton = document.querySelector('.filter_button');
   // Add a querySelector that targets your filter button here
 
   let currentList = []; // this is "scoped" to the main event function
@@ -54,7 +60,17 @@ async function mainEvent() { // the async keyword means we can make API requests
     console.table(currentList); 
   });
 
+  filterButton.addEventListener('click', (event) => {
+    console.log('clicked FilterButton');
 
+    const formData = new FormData(mainForm);
+    const formProps = Object.fromEntries(formData);
+
+    console.log(formProps);
+    const newList = filterList(currentList, formProps.resto);
+
+    console.log(newList);
+  });
   /*
     Now that you HAVE a list loaded, write an event listener set to your filter button
     it should use the 'new FormData(target-form)' method to read the contents of your main form
